64bd727b51233c396dafaec8e604fb57295582d9
yury deshin <j.deshin@hotmail.com>
2018-01-29 14:55:14 +0300

    ---

--- YamlDotNetProcessor/YamlDotNetYamlProcessor.cs
+++ YamlDotNetProcessor/YamlDotNetYamlProcessor.cs
@@ -32,5 +32,58 @@ namespace YamlDotNetProcessor
             return ValueFactory.Create();
         }
 
+        public static object GetValue(object source)
+        {
+            if (source == null)
+                return source;
+
+            if (source is List<object>)
+            {
+                foreach (object element in (List<object>)source)
+                {
+                    object value = GetValue(element);
+                }
+
+                return source;
+            }
+
+            if (source is Dictionary<object, object>)
+            {
+                foreach (var element in (Dictionary<object, object>)source)
+                {
+                    object key = GetValue(element.Key);
+                    object value = GetValue(element.Value);
+                }
+
+                return source;
+            }
+
+            if (source is bool)
+                return source;
+
+            // Число
+            if (source is sbyte
+                || source is byte
+                || source is short
+                || source is ushort
+                || source is int
+                || source is uint
+                || source is long
+                || source is ulong
+                || source is float
+                || source is double
+                || source is decimal
+               )
+
+                return source;
+
+            // Дата
+            if (source is DateTime)
+                return source;
+
+            // Строка или нечто другое
+            return source.ToString();
+        }
+
     }
 }
