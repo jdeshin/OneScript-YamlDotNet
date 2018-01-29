71fa66af23242dedc69e036171a97656b9220fdd
yury deshin <j.deshin@hotmail.com>
2018-01-29 11:26:22 +0300

    Add project files.

---
+++ YamlDotNetProcessor/YamlDotNetProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library;
+
+using YamlDotNet;
+using YamlDotNetProcessor;
+
+namespace YamlDotNetProcessor
+{
+    [ContextClass("YamlПроцессорYamlDotNet", "YamlDotNetProcessor")]
+    public class YamlDotNetProcessor : AutoContext<YamlDotNetProcessor>
+    {
+        public YamlDotNetProcessor()
+        {
+        }
+
+        [ScriptConstructor(Name = "Без параметров")]
+        public static IRuntimeContextInstance Constructor()
+        {
+            return (IRuntimeContextInstance)new YamlDotNetProcessor();
+        }
+    }
+}
